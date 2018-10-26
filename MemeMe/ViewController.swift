6764f1f65c2b22ad17810dc2d5923ed65a15b3c3
José Naves <josenaves@gmail.com>
2018-10-25 22:36:54 -0300

    chore: Add initial Xcode project files

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by José Naves Moura Neto on 25/10/18.
+//  Copyright © 2018 José Naves Moura Neto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
