bc242705d96413e7f0b53a132f65ee5bff6f53ab
José Naves <josenaves@gmail.com>
2018-11-05 19:06:08 -0200

    feat: Add meme model

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by José Naves Moura Neto on 04/11/18.
+//  Copyright © 2018 José Naves Moura Neto. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
